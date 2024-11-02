7ed89378af09d4f31cf04b150c97c3867ab3bc77
natakurage <uzogamuzol@gmail.com>
2024-11-02 14:15:14 +0900

    リストにtitle

--- src/app/lists/[id]/page.tsx
+++ src/app/lists/[id]/page.tsx
@@ -1,6 +1,18 @@
 import ArticleList from "@/components/ArticleList";
 import { getList } from "@/lib/contentful";
 import { notFound } from "next/navigation";
+import data from "@/app/data/data.json";
+
+export async function generateMetadata ({ params }: { params: { id: string } }) {
+  const { id } = params;
+  const list = await getList(id, false);
+  if (!list) { 
+    notFound();
+  }
+  return {
+    title: `記事リスト "${list.title}"` + " - " + data.siteName,
+  };
+}
 
 export default async function ListsPage(
   { params, searchParams }
--- src/app/lists/page.tsx
+++ src/app/lists/page.tsx
@@ -1,5 +1,11 @@
 import ListList from "@/components/ListList";
 import { getLists } from "@/lib/contentful";
+import { Metadata } from "next";
+import data from "@/app/data/data.json";
+
+export const metadata: Metadata = {
+  title: "記事リスト一覧 - " + data.siteName,
+};
 
 export default async function ListPage({ searchParams }: { searchParams: { page?: string } }) {
   const { page = 1 } = searchParams;
