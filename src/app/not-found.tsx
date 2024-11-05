266b22fbc19c3f23e0988da741f66e648ecc371c
natakurage <uzogamuzol@gmail.com>
2024-11-05 16:43:25 +0900

    not-foundを自作

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+import data from "@/app/data/data.json";
+
+export default function NotFound() {
+  return (
+    <div className="prose dark:!prose-invert">
+      <h1>404 Not Found</h1>
+      <p>{data.notFoundMessage}</p>
+    </div>
+  );
+}
