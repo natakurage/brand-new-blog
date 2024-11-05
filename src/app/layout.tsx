5645936e2ee226458b96a15b7af206c3650e827e
natakurage <uzogamuzol@gmail.com>
2024-11-05 14:10:47 +0900

    edgeをまたやめる

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,7 +33,7 @@ export const metadata: Metadata = {
 
 export const dynamic = "force-dynamic";
 
-export const runtime = "edge";
+// export const runtime = "edge";
 
 export default function RootLayout({
   children,
