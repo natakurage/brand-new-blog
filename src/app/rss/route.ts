74d0815f4585c56e9556cd211b6689a9ca33098b
natakurage <uzogamuzol@gmail.com>
2024-11-05 14:03:36 +0900

    rssを自前化してedgeに対応？

--- src/app/rss/route.ts
+++ src/app/rss/route.ts
@@ -1,5 +1,4 @@
 import { getPosts } from "@/lib/contentful";
-import rss from "rss";
 import data from "@/app/data/data.json";
 import removeMd from "remove-markdown";
 
@@ -10,25 +9,34 @@ export async function GET(request: Request) {
     return new Response(null, { status: 404 });
   }
 
-  const feed = new rss({
-    title: data.siteName,
-    description: data.description,
-    site_url: url,
-    feed_url: `${url}/rss.xml`,
-    language: "ja",
-  });
-
   const { posts } = await getPosts({});
 
-  posts.forEach((post) => {
-    feed.item({
-      title: post.title,
-      description: removeMd(post.body).slice(0, 100) + "...",
-      url: `${url}/articles/${post.slug}`,
-      author: data.author,
-      date: post.createdAt,
-    });
-  });  
+  const items = posts.map(post => `<item>
+      <title><![CDATA[${post.title}]]></title>
+      <description><![CDATA[${removeMd(post.body).slice(0, 100) + "..."}]]></description>
+      <link>${url}/articles/${post.slug}</link>
+      <guid isPermaLink="false">${url}/articles/${post.slug}</guid>
+      <dc:creator><![CDATA[${data.author}]]></dc:creator>
+      <pubDate>${new Date(post.createdAt).toUTCString()}</pubDate>
+    </item>`
+  );
 
-  return new Response(feed.xml({ indent: true }));
+  const channel = `<channel>
+  <title>${data.siteName}</title>
+  <description>${data.description}</description>
+  <link>${url}</link>
+  <generator></generator>
+  <lastBuildDate>${new Date().toUTCString()}</lastBuildDate>
+  <atom:link href="${url}/rss" rel="self" type="application/rss+xml"/>
+  <language><![CDATA[ja]]></language>
+  ${items.join("\n")}
+  </channel>`;
+
+  const rss = `<rss xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:atom="http://www.w3.org/2005/Atom" version="2.0">
+  ${channel}
+  </rss>`;
+
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>${rss}`;
+
+  return new Response(xml);
 }
