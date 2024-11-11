0ce8ba03da0e0554ea884a32ecf3b0810114103d
natakurage <uzogamuzol@gmail.com>
2024-11-12 00:28:34 +0900

    config edgeを試す

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,6 +29,9 @@ export const metadata: Metadata = {
 
 export const runtime = "edge";
 
+export const config = {
+  runtime: 'edge',
+};
 export default function RootLayout({
   children,
 }: Readonly<{
