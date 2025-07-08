5f8bcc9e3d6bc88aea560aa1316ce7178b35bc1e
natakurage <natakurage@natakurage.cc>
2025-07-08 16:47:30 +0900

    WebSiteをHomeに移動

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,38 @@ import ItemList from "@/components/ItemList";
 import { BlogPostManager } from "@/lib/contentful";
 import { Metadata } from "next";
 import data from "@/app/data/data.json";
+import Script from "next/script";
+import type {  WebSite, WithContext } from "schema-dts";
 
 export const metadata: Metadata = {
   title: data.siteName,
 };
+
+function JsonLD() {
+  const jsonLd: WithContext<WebSite> = {
+    "@context": "https://schema.org",
+    "@type": "WebSite",
+    name: data.siteName,
+    url: process.env.NEXT_PUBLIC_ORIGIN,
+    description: data.description,
+    image: new URL("/banner.png", process.env.NEXT_PUBLIC_ORIGIN).href,
+    potentialAction: {
+      "@type": "SearchAction",
+      target: new URL("/search?q={search_term_string}", process.env.NEXT_PUBLIC_ORIGIN).href,
+      // @ts-expect-error: 'query-input' is not in the schema-dts types but required by schema.org
+      "query-input": "required name=search_term_string"
+    },
+  };
+
+  return (
+    <Script
+      id="json-ld"
+      type="application/ld+json"
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+    />
+  );
+}
 
 export default async function Home({ searchParams }: { searchParams: { page?: string } }) {
   const { page = 1 } = searchParams;
@@ -15,6 +43,9 @@ export default async function Home({ searchParams }: { searchParams: { page?: st
     filter: { order: "-sys.createdAt" },
   });
   return (
-    <ItemList items={posts} page={pageNum} total={total} limit={limit} />
+    <>
+      <JsonLD />
+      <ItemList items={posts} page={pageNum} total={total} limit={limit} />
+    </>
   );
 }
