0f40339cfc58875347b64f8ac8be020fcdf4eff1
natakurage <natakurage@natakurage.cc>
2025-07-10 19:29:51 +0900

    エラーメッセージを返す

--- src/app/api/list-navigator/route.ts
+++ src/app/api/list-navigator/route.ts
@@ -1,6 +1,10 @@
 import { isItemListManagerMapKeys, ItemListManagerMap } from "@/lib/contentful";
 import { listNavigatorInfo } from "@/lib/models";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+
+const responseWithError = (error: string, status: number) => {
+  return new NextResponse(JSON.stringify({ error }), { status });
+};
 
 export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams;
@@ -9,19 +13,19 @@ export async function GET(request: NextRequest) {
   const managerType = searchParams.get("type");
   const useSlug = searchParams.get("useSlug");
   // check params
-  if (!key || !currentSlug || !managerType) return new Response(null, { status: 400 });
+  if (!key || !currentSlug || !managerType) return responseWithError("Missing parameters", 400);
   // check manager type
-  if (!isItemListManagerMapKeys(managerType)) return new Response(null, { status: 400 });
+  if (!isItemListManagerMapKeys(managerType)) return responseWithError("Invalid manager type", 400);
   const manager = ItemListManagerMap.get(managerType);
-  if (!manager) return new Response(null, { status: 400 });
+  if (!manager) return responseWithError("Type not found", 400);
   // get list
   const list = useSlug === "true" ? await manager.getBySlug(key) : await manager.get(key);
-  if (!list) return new Response(null, { status: 404 });
+  if (!list) return responseWithError("List not found", 404);
   // get info
   const { title, items } = list;
   // check index
   const currentId = items.findIndex((item) => item.slug === currentSlug);
-  if (currentId === -1) return new Response(null, { status: 404 });
+  if (currentId === -1) return responseWithError("Item not found", 404);
   const prev = items[currentId - 1] ?? null;
   const next = items[currentId + 1] ?? null;
   const navigatorInfo: listNavigatorInfo = {
@@ -38,5 +42,5 @@ export async function GET(request: NextRequest) {
       typeUrl: next.typeUrl
     }
   };
-  return new Response(JSON.stringify(navigatorInfo), { status: 200 });
+  return new NextResponse(JSON.stringify(navigatorInfo), { status: 200 });
 }
